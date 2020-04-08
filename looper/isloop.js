ae79de2daafd5a34da0e33efecb13b20a3fd5ceb
Damir Robert <damirhenrirobert@gmail.com>
2020-04-08 16:52:20 -0400

    completed min-jump

--- looper/isloop.js
+++ looper/isloop.js
@@ -2,9 +2,27 @@
 
 //Complete this algo
 const isLoop = (linkedlist) => {
+    
+    let current = linkedlist.head
+    let soFar = []
 
+    while (current.next){
+        if (soFar.includes(current)) {
+            return true;
+        } else {
+            soFar.push(current)
+            current = current.next
+        }
+    }
+    return false
 };
 
+/*
+Iterate through the list
+Store each new value in an array
+If a new value already exists in the array, return true
+If there is no next value, return false
+*/
 
 /*
 EXTRA CREDIT:
